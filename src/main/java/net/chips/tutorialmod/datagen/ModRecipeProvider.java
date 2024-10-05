26aacf9283eba40210b5d10ee43f07d0f0b58eec
Gustas11 <vilimas.gustas@gmail.com>
2024-10-05 21:02:44 +0100

    Added data generation

---
+++ src/main/java/net/chips/tutorialmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,45 @@
+package net.chips.tutorialmod.datagen;
+
+import net.chips.tutorialmod.block.ModBlocks;
+import net.chips.tutorialmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static List<ItemConvertible> RUBY_SMELTABLE = List.of(ModItems.RAW_RUBY,
+            ModBlocks.RUBY_ORE, ModBlocks.DEEPSLATE_RUBY_ORE, ModBlocks.NETHER_RUBY_ORE, ModBlocks.ENDSTONE_RUBY_ORE);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLE, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLE, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 100, "ruby");
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RUBY, RecipeCategory.DECORATIONS,
+                ModBlocks.RUBY_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.RUBY_ORE, 1)
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .input('S', Items.STONE)
+                .input('R', ModItems.RAW_RUBY)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .criterion(hasItem(ModItems.RAW_RUBY), conditionsFromItem(ModItems.RAW_RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.RUBY_ORE)));
+    }
+}
